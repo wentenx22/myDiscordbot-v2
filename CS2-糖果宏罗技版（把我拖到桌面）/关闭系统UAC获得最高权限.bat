cca82de7419a03446232ca7880e7842a00e74aa6
wentenx22 <wentenx22@gmail.com>
2026-01-04 17:13:39 +0800

    Update: Latest bot changes

--- CS2-糖果宏罗技版（把我拖到桌面）/关闭系统UAC获得最高权限.bat
+++
@@ -1,6 +0,0 @@
-%windir%\system32\cmd.exe /k %windir%\System32\reg.exe add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
-
-echo.echo 请重启电脑生效
-
-echo.&pause
-
